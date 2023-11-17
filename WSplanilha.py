cc98a7e9b7a537823ce7ea8c0a2da9b318ccf630
Emanuelle J <74027479+m4nella@users.noreply.github.com>
2023-11-17 11:20:45 -0300

    🗨 | Comment

--- WSplanilha.py
+++ WSplanilha.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import openpyxl
 
+# html --> sem url --> arquivo local - apenas
 with open('C:/Users/55137877840/Desktop/FPOO-Aula16-WebScraping-Pedidos.html', 'r', encoding='utf-8') as arquivo:
     conteudo_html = arquivo.read()
 f = open('file.py')
@@ -16,6 +17,6 @@ for indice_linha, linha in enumerate(tabela.find_all('tr')):
     for indice_coluna, celula in enumerate(linha.find_all(['th', 'td'])):
         folha.cell(row=indice_linha + 1, column=indice_coluna + 1, value=celula.get_text(strip=True))
 
-#salvando em excel
+# salvando em excel
 planilha.save('listinhapedidinhos.xlsx')
 
